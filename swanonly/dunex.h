67a9eac1ac45718bade91a3fcd3cbd6eb9e72a19
EJ Rainville <erainvil@uw.edu>
2021-08-23 14:28:09 -0700

    Added all files from the previous directory structure to the dunexCOAWST Repo in the new structure

---
+++ swanonly/dunex.h
@@ -0,0 +1,20 @@
+/*
+*******************************************************************************
+** Copyright (c) 2002-2010 The ROMS/TOMS Group                               **
+**   Licensed under a MIT/X style license                                    **
+**   See License_ROMS.txt                                                    **
+*******************************************************************************
+**
+** Options for DUNEX Coupled Test, waves-ocean (SWAN/ROMS) two-way coupling.
+** Based on Coupling Test
+** Application flag:   DUNEX
+** Input scripts:      coupling_dunex.in
+**                     ocean_dunex.in
+**                     swan_dunex.in
+*/
+
+#define SWAN_MODEL
+
+
+
+
